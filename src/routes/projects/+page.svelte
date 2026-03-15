ff01649bed9652231b8b483f905e3f3ad2295a31
SmoothTurtle872 <whatleyfamily1@gmail.com>
2026-03-15 12:49:07 +0930

    added an under cosntruction  label to the projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,4 +4,17 @@
 </script>
 
 <Header page="projects" tagline="My Projects" />
+<div>
+    <h1>THIS PAGE IS STILL UNDER CONSTRUCTION</h1>
+    <p>
+        Sorry for the inconvinience, but check the footer for links to my github
+        and itch.io page
+    </p>
+</div>
 <Footer />
+
+<style>
+    div {
+        text-align: center;
+    }
+</style>
